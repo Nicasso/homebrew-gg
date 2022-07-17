446bd260ba0fb609ebfcac2b56675b2080198edf
Nico de Groot <nico@nicasso.nl>
2022-07-17 10:41:48 +0200

    Fixed package name

--- gg.rb
+++ gg.rb
@@ -1,4 +1,4 @@
-class GG < Formula
+class Gg < Formula
     desc "GG speeds up the creation of those pesky single commit merge requests."
     homepage "https://github.com/nicasso/gg"
     url "https://github.com/Nicasso/gg/archive/refs/tags/v0.0.1.tar.gz"
