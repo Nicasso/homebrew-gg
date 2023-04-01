177f65d1635b1d1b57a42d5996f2fa45dda4e65b
Nicasso <nico@nicasso.nl>
2023-04-01 13:52:41 +0200

    Updating to v1.0.2

--- gg.rb
+++ gg.rb
@@ -1,8 +1,8 @@
 class Gg < Formula
     desc "GG speeds up the creation of those pesky single commit merge requests."
     homepage "https://github.com/nicasso/gg"
-    url "https://github.com/Nicasso/gg/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "91f7dfeee9e97920a6635ac4f5d932ec1e7ec59826e9ad180aed2efe33f71c22"
+    url "https://github.com/Nicasso/gg/archive/refs/tags/v1.0.2.tar.gz"
+    sha256 "6458951226ec40814701569adeafbbffdbb18b68b8ded8b940733d0466b83f78"
     license "GNU General Public License V3"
 
     def install
